8cb1d231a9446d3256557c6cfca3b72deb5bcae5
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-21 10:56:00 +1300

    initialise project

---
+++ heavenly-visions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace heavenly_visions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
