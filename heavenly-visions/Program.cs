d4e05b54b104f40dcea8cc91978195fa3976fdf8
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-24 13:19:13 +1300

    feat: basic skeleton structure

--- heavenly-visions/Program.cs
+++ heavenly-visions/Program.cs
@@ -8,8 +8,25 @@
             string[] Tour1 = new string[10];
             string[] Tour2 = new string[10];
             string[] Tour3 = new string[10];
-
-
+            int mainMenu = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("sample text");
+                mainMenu = Convert.ToInt32(Console.ReadLine());
+                switch(mainMenu)
+                {
+                    case 0:
+                        //exit
+                        break;
+                    case 1:
+                        //enter user
+                        break;
+                    case 2:
+                        //view tours
+                        break;
+                }
+            } while (mainMenu != 0);
         }
     }
 }
